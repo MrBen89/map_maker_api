d219433659987e182d7e3784e57088391ce3002c
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2025-06-30 20:58:34 +0900

    updated query to include tilemap for square maps

--- app/controllers/square_maps_controller.rb
+++ app/controllers/square_maps_controller.rb
@@ -1,6 +1,8 @@
 class SquareMapsController < ApplicationController
   def show
-    @tiles = SquareMapTile.where("square_map_id = ?", params["id"])
-    render json: @tiles.to_json
+    @tiles = SquareMapTile.joins("INNER JOIN square_tiles ON square_map_tiles.square_tile_id = square_tiles.id")
+    .where("square_map_id = ?", params["id"]).select("square_map_tiles.*, square_tiles.*")
+    
+    render json: @tiles
   end
 end
